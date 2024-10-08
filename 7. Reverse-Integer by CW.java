df8bac09eed005511d9b6dc8ac3e4a73fb0f0840
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2024-10-08 10:56:09 +0530

    Update 7. Reverse-Integer by CW.java

--- 7. Reverse-Integer by CW.java
+++ 7. Reverse-Integer by CW.java
@@ -1,13 +1,12 @@
 # 7.Reverse Integer
-  O(logN)
+  Solution-1.
+  [ O(logN) ]
 
 class Solution{
   public int reverse(int x){
-
   boolean isNegative = x<0;
   x = Math.abs(x);
-  
-
+    
   String rev = new StringBuilder(Integer.toString(x)).reverse().toString();
 
  try{
@@ -17,7 +16,41 @@ class Solution{
  catch(Exception e){
     return 0;
     }
-
   }
-
 }
+
+---------------------------------------------------------------------
+
+Solution-2.
+[ O(N) ]
+
+class Solution{
+  public int reverse(int x){
+
+    long rev = 0;
+    int mod = 0;
+
+    while(x!=0){
+      mod = x % 10;
+      rev = rev * 10 + mod;
+      x/= 10;
+    }
+
+    if(rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE){
+      return 0;
+    }
+    return (int) rev;
+  }
+}
+
+
+
+
+
+
+
+
+
+  
+
+  
