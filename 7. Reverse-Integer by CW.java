8d38a0594c51a0713b6c4f24e54849f1b0c36943
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2025-01-02 10:20:00 +0530

    Update 7. Reverse-Integer by CW.java

--- 7. Reverse-Integer by CW.java
+++ 7. Reverse-Integer by CW.java
@@ -45,6 +45,39 @@ class Solution{
 
 
 
+-----------------------------------------------------
+  
+Solution-3.
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+
+        //condition : -2^(31) <= x <= 2^(31 - 1)
+        //Minimum Value: Integer.MIN_VALUE = -2,147,483,648
+        //Maximum Value: Integer.MAX_VALUE = 2,147,483,647
+
+        while (x != 0) {
+            int mod = x % 10;
+
+            // rev number can increse and cross limit of max ..
+            // and we mulitify by 10 so check /10  and for max last digit is 7 so % > 7 as +mod to rev
+            if(rev > Integer.MAX_VALUE / 10 || ( rev == Integer.MAX_VALUE && mod > 7)){
+                return 0;
+            }
+
+            // rev number can increse and cross limit of min ..
+            // and we mulitify by 10 so check /10  and for min last digit is -8 so % < -8 as +mod to rev
+            if(rev < Integer.MIN_VALUE / 10 || ( rev == Integer.MIN_VALUE && mod < -8)){
+                return 0;
+            }
+
+            rev = rev * 10 + mod;
+            x = x / 10;
+        }
+
+        return rev;
+    }
+}
 
 
 
