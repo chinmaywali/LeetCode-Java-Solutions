4cf83ddabe85f59474238186ce885362d43d1f94
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2025-01-09 15:30:43 +0530

    Update 26. Remove Duplicates from Sorted Array by cw.java

--- 26. Remove Duplicates from Sorted Array by cw.java
+++ 26. Remove Duplicates from Sorted Array by cw.java
@@ -18,3 +18,26 @@ class Solution {
       return s.size();
     }
 }
+
+
+--------------------------------------------------------------------------------
+
+Optimal solution :
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        if(nums == null || nums.length < 0){
+            return 0;
+        }
+        
+        int k = 1; 
+        for(int i = 1 ; i < nums.length ; i++){
+            if(nums[i] != nums[i-1]){ //0 1 1 2 3
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
